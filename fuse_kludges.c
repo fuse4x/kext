248ee6cea3eb29ceef757b295a45e28315bf5ede
Erik Larsson <catacombae@gmail.com>
2011-09-04 10:57:02 +0200

    Add a Darwin 11 definition of fuse_kludge_vnode.
    This definition is actually binary compatible with the Darwin 10 version
    with regard to the relevant fields as the only thing that changed
    between Darwin 10 and Darwin 11 is the addition of a new member at the
    end of the struct, but it is added for completeness.

--- fuse_kludges.c
+++ fuse_kludges.c
@@ -11,19 +11,19 @@
 /* The shop of horrors. */
 
 /*
- * # 10.5        10.6-32        10.6-64
+ * # 10.5        10.6-32     10.6-64     10.7-32     10.7-64
  *
  * # sizeof(struct vnode)
- *   144         148            248
+ *   144         148         248         152         256
  *
  * # offsetof(struct vnode, v_lflag)
- *   48          48             88
+ *   48          48          88          48          88
  *
  * # offsetof(struct vnode, v_name)
- *   112         116            184
+ *   112         116         184         116         184
  *
  * # offsetof(struct vnode, v_parent)
- *   116         120            192
+ *   116         120         192         120         192
  */
 
 struct fuse_kludge_vnode_9 {
@@ -55,10 +55,32 @@ struct fuse_kludge_vnode_10 {
 } __attribute__ ((packed));
 #endif
 
+struct fuse_kludge_vnode_11 {
+    void    *dummy0[10];
+    char     dummy1[8];
+    uint16_t v_lflag;
+    void    *dummy2[7];
+    char     dummy3[38];
+    char    *v_name;
+    vnode_t  v_parent;
+    void    *dummy4[7];
+} __attribute__ ((packed));
+
 void
 fuse_kludge_exchange(vnode_t v1, vnode_t v2)
 {
-    if (version_major > 9) {
+    if (version_major > 10) {
+        char *tmp_v_name = ((struct fuse_kludge_vnode_11 *)v1)->v_name;
+        ((struct fuse_kludge_vnode_11 *)v1)->v_name =
+            ((struct fuse_kludge_vnode_11 *)v2)->v_name;
+        ((struct fuse_kludge_vnode_11 *)v2)->v_name = tmp_v_name;
+
+        vnode_t tmp_v_parent = ((struct fuse_kludge_vnode_11 *)v1)->v_parent;
+        ((struct fuse_kludge_vnode_11 *)v1)->v_parent =
+            ((struct fuse_kludge_vnode_11 *)v2)->v_parent;
+        ((struct fuse_kludge_vnode_11 *)v2)->v_parent = tmp_v_parent;
+    }
+    else if (version_major > 9) {
         char *tmp_v_name = ((struct fuse_kludge_vnode_10 *)v1)->v_name;
         ((struct fuse_kludge_vnode_10 *)v1)->v_name =
             ((struct fuse_kludge_vnode_10 *)v2)->v_name;
