679ae26f2ea97e184871881bf0c537347f4ce018
Erik Larsson <catacombae@gmail.com>
2012-02-25 07:46:16 +0100

    fuse_kludges.h: Corrected Darwin 11 kludge vnode definition.
    Apple changed the definition of lck_mtx_t in Darwin 11 (Mac OS X 10.7),
    meaning that offsets in the 'kludge' vnode definition were incorrect.
    So Darwin 11's struct vnode is not binary compatible with Darwin 10's as
    previously assumed.
    
    This incorrect definition causes problems for fuse_kludge_exchange and
    fuse_kludge_vnode_isrecycled. In the former case, memory corruption
    could occur and undefined behaviour may follow (serious issue). In the
    latter case, the function would return the wrong result.

--- fuse_kludges.c
+++ fuse_kludges.c
@@ -14,20 +14,21 @@
  * # 10.5        10.6-32     10.6-64     10.7-32     10.7-64
  *
  * # sizeof(struct vnode)
- *   144         148         248         152         256
+ *   144         148         248         148         248
  *
  * # offsetof(struct vnode, v_lflag)
- *   48          48          88          48          88
+ *   48          48          88          44          80
  *
  * # offsetof(struct vnode, v_name)
- *   112         116         184         116         184
+ *   112         116         184         112         176
  *
  * # offsetof(struct vnode, v_parent)
- *   116         120         192         120         192
+ *   116         120         192         116         184
  */
 
 struct fuse_kludge_vnode_9 {
-    char     dummy0[48];
+    char     v_lock[12];
+    char     dummy0[36];
     uint16_t v_lflag;
     char     dummy1[62];
     char    *v_name;
@@ -37,7 +38,8 @@ struct fuse_kludge_vnode_9 {
 
 #if __LP64__
 struct fuse_kludge_vnode_10 {
-    char     dummy0[88];
+    char     v_lock[24];
+    char     dummy0[64];
     uint16_t v_lflag;
     char     dummy1[94];
     char    *v_name;
@@ -46,7 +48,8 @@ struct fuse_kludge_vnode_10 {
 } __attribute__ ((packed));
 #else
 struct fuse_kludge_vnode_10 {
-    char     dummy0[48];
+    char     v_lock[12];
+    char     dummy0[36];
     uint16_t v_lflag;
     char     dummy1[66];
     char    *v_name;
@@ -56,7 +59,8 @@ struct fuse_kludge_vnode_10 {
 #endif
 
 struct fuse_kludge_vnode_11 {
-    void    *dummy0[10];
+    void    *v_lock[2];
+    void    *dummy0[7];
     char     dummy1[8];
     uint16_t v_lflag;
     void    *dummy2[7];
